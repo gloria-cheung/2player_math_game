ff49c0a69ffee0e66c3648042ff46910134f2d27
Gloria <gloriacheung812@gmail.com>
2022-08-23 22:41:26 +0000

    require turn and lives class and define func start to play the game

---
+++ main.rb
@@ -0,0 +1,41 @@
+require './turn'
+require './lives'
+
+def start
+  player = 1
+  turn = Turn.new
+  lives = Lives.new
+
+  while lives.score1 != 0 && lives.score2 != 0
+    puts "Player #{player}: What does #{turn.num1} plus #{turn.num2} equal?"
+
+    print "> "
+    choice = $stdin.gets.chomp
+
+    if choice.to_i == (turn.num1 + turn.num2)
+      puts "Player #{player}: YES! You are correct."
+      puts "P1: #{lives.score1}/3 vs P2 #{lives.score2}/3"
+    else
+      puts "Player #{player}: Seriously? No!"
+      if player == 1
+        lives.score1 -= 1
+      else
+        lives.score2 -= 1
+      end
+      puts "P1: #{lives.score1}/3 vs P2: #{lives.score2}/3"
+    end
+
+    if player == 1
+      player = 2
+    elsif player == 2
+      player = 1
+    end
+
+    puts "----- NEW TURN -----"
+    turn = Turn.new
+  
+  end 
+
+end
+
+start
